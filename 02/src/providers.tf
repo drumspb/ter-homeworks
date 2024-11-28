38b9f5d9389fc497a053368e632e8896a8ef0e07
Кирилл Дромашко <spbdrumspb@gmail.com>
2024-11-28 22:55:42 +0300

    hw-02.1

--- 02/src/providers.tf
+++ 02/src/providers.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "yandex" {
   # token     = var.token
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
+  cloud_id                 = "b1golipav4ipa0ltifko" 
+  folder_id                = "b1goiv8hbuqegdqk3k0r" 
   zone                     = var.default_zone
-  service_account_key_file = file("~/.authorized_key.json")
+  service_account_key_file = file("~/authorized_key.json")
 }
--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -33,6 +33,6 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINa/Eyvhmr2G9H7AKbLP4/peDIF/98b/yeE4ajEqOdZ0 <Git>"
   description = "ssh-keygen -t ed25519"
 }
